65ffc80443990d3f7851f78c9076d5db45cf843a
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2025-10-10 14:27:17 +0200

    Legger tilbake loggeren og logger trykk på mikrofrontenden

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,10 +1,12 @@
 import type { AmplitudeEvent } from "@navikt/nav-dekoratoren-moduler";
 import { getAnalyticsInstance } from "@navikt/nav-dekoratoren-moduler";
+import { logger } from "./logger";
 
 type ExtendedAmpltitudeEvent = AmplitudeEvent<"navigere", { lenketekst: string }>;
 
 const analyticsLogger = getAnalyticsInstance<ExtendedAmpltitudeEvent>("pam-min-side-microfrontend");
 
 export const logEvent = async (lenketekst: string) => {
+  logger.info(`Noen har trykket på mikrofrontenden som føreer til ${lenketekst}`);
   await analyticsLogger("navigere", { lenketekst: lenketekst });
 };
