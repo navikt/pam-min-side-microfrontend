21f5e2fa1e70d6cfa2e300961db179d428c3035b
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2025-10-13 09:34:17 +0200

    Justerer på event-navn på toppnivå

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -5,10 +5,6 @@ type ExtendedAmpltitudeEvent = AmplitudeEvent<"navigere", { lenketekst: string }
 
 const analyticsLogger = getAnalyticsInstance<ExtendedAmpltitudeEvent>("pam-min-side-microfrontend");
 
-export const logEvent = async (lenketekst: string) => {
-  await analyticsLogger("navigere", { lenketekst: lenketekst });
-};
-
 export const logClickEvent = async () => {
   await analyticsLogger("min-cv-lenke");
 };
